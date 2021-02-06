68a3696e5ba581ee8aa9ef1942faaa95065f0a19
AyanNandaGoswami <ayan02472@gmail.com>
2021-02-06 18:03:49 +0530

    rest_framework and cors headers setup

--- my_blogApp/my_blogApp/settings.py
+++ my_blogApp/my_blogApp/settings.py
@@ -37,9 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# cors headers setup
+CORS_ALLOW_ALL_ORIGINS = True
+
